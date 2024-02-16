92eb750fa9acff3de66dcf37494e0fd1d67df032
Juliette Lega <legajuliette@gmail.com>
2024-02-16 10:32:34 +0100

    ajout photo banniere et js menu pour icone fleche

--- js/nav.js
+++ js/nav.js
@@ -20,23 +20,63 @@ let sousInfos = document.querySelector('.sousInfos');
 let flecheQuizz = document.querySelector('.flecheQuizz');
 let flecheInfos = document.querySelector('.flecheInfos');
 
+let fleche1 = document.getElementById('iconeFlecheMenu1');
+let fleche2 = document.getElementById('iconeFlecheMenu');
+
 
 quizz.addEventListener("click", () => {
   if(getComputedStyle(sousQuizz).display != "block") {
-    sousQuizz.style.display = "block";
-    flecheQuizz.style.rotate = "90deg";
+    
+    if(fleche2.classList.contains('icone-fleche-ferme')) {
+      fleche2.classList.remove('icone-fleche-ferme');
+    }
+      sousQuizz.style.display = "block";
+      fleche2.classList.add('icone-fleche-ouvre');
+      fleche2.style.animationPlayState = "running";
+    // flecheQuizz.style.rotate = "90deg";
   } else {
     sousQuizz.style.display = "none";
-    flecheQuizz.style.rotate = "0deg";
+    fleche2.classList.remove('icone-fleche-ouvre');
+    fleche2.classList.add('icone-fleche-ferme');
+    fleche2.style.animationPlayState = "running";
+    // flecheQuizz.style.rotate = "0deg";
   }
 })
 
 info.addEventListener("click", () => {
   if(getComputedStyle(sousInfos).display != "block") {
+
+    if(fleche1.classList.contains('icone-fleche-ferme')) {
+      fleche1.classList.remove('icone-fleche-ferme');
+    }
+
     sousInfos.style.display = "block";
-    flecheInfos.style.rotate = "90deg";
+    fleche1.classList.add('icone-fleche-ouvre');
+    fleche1.style.animationPlayState = "running";
+    // flecheInfos.style.rotate = "90deg";
   } else {
     sousInfos.style.display = "none";
-    flecheInfos.style.rotate = "0deg";
+    fleche1.classList.remove('icone-fleche-ouvre');
+    fleche1.classList.add('icone-fleche-ferme');
+    fleche1.style.animationPlayState = "running";
+    // flecheInfos.style.rotate = "0deg";
+  }
+})
+
+
+let scroll1 = document.getElementById('img-left-scroll');
+let scroll2 = document.getElementById('img-right-scroll');
+
+scroll2.addEventListener('click', () => {
+  if(getComputedStyle(scroll1).display != "block") {
+    scroll1.style.display = "block";
+    scroll2.style.display = "none";
+  }
+})
+
+scroll1.addEventListener('click', () => {
+  if(getComputedStyle(scroll2).display != "block") {
+    scroll2.style.display = "block";
+    scroll1.style.display = "none";
   }
 })
